75ca1e5734c55ef29e9b07879e11697bfc2f80f4
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-02-14 08:30:15 +0700

    cardview

--- app/src/main/java/com/example/profileschool/BebasActivity.java
+++ app/src/main/java/com/example/profileschool/BebasActivity.java
@@ -1,17 +1,14 @@
 package com.example.profileschool;
 
-import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class BebasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_bebas);
     }
 }
